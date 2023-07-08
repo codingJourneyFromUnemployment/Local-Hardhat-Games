8d546a6f24f17d892f63f48c5cce92135cc2f258
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-07-08 15:07:39 +0800

    init project

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "localhost",
+  networks: {
+    sepolia: {
+      url:process.env.SEPOLIA_RPC_URL, 
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111, 
+    },
+    goerli: {
+      url:process.env.GOERLI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5,
+      constructorArgs: [process.env.EmitWinner_Goerli_Address]
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+      constructorArgs: [process.env.EmitWinner_Local_Address]
+    }
+  },
   solidity: "0.8.17",
-  defaultNetwork: 'localhost'
 };
